479d3ff190e45f131a5394f3bae8d84f3d66ecd3
panukettu <panu@0xp.lol>
2023-04-25 23:06:32 +0300

    add more contracts

--- src/MinterTypes.sol
+++ src/MinterTypes.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.14;
 
-import {AggregatorV2V3Interface} from "../vendor/flux/interfaces/AggregatorV2V3Interface.sol";
-import {FixedPoint} from "../libs/FixedPoint.sol";
-import {IKreskoAssetAnchor} from "../kreskoasset/IKreskoAssetAnchor.sol";
-import {LibAssetUtility} from "./libs/LibAssetUtility.sol";
+import {AggregatorV2V3Interface} from "./interfaces/IFlux.sol";
+import {FixedPoint} from "./libs/FixedPoint.sol";
+import {IKreskoAssetAnchor} from "./interfaces/IKreskoAssetAnchor.sol";
 
 /* solhint-disable state-visibility */
 
@@ -119,7 +118,7 @@ struct KrAsset {
     FixedPoint.Unsigned openFee;
     bool exists;
 }
-using LibAssetUtility for KrAsset global;
+
 /**
  * @notice Information on a token that can be used as collateral.
  * @dev Setting the factor to zero effectively makes the asset useless as collateral while still allowing
@@ -138,7 +137,6 @@ struct CollateralAsset {
     uint8 decimals;
     bool exists;
 }
-using LibAssetUtility for CollateralAsset global;
 
 /// @notice Configuration for pausing `Action`
 struct Pause {
@@ -150,4 +148,62 @@ struct Pause {
 /// @notice Safety configuration for assets
 struct SafetyState {
     Pause pause;
+}
+
+/**
+ * @title Storage layout for the minter state
+ * @author Kresko
+ */
+struct MinterState {
+    /* -------------------------------------------------------------------------- */
+    /*                               Initialization                               */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Initialization version
+    uint256 initializations;
+    bytes32 domainSeparator;
+    /* -------------------------------------------------------------------------- */
+    /*                           Configurable Parameters                          */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice The recipient of protocol fees.
+    address feeRecipient;
+    /// @notice The factor used to calculate the incentive a liquidator receives in the form of seized collateral.
+    FixedPoint.Unsigned liquidationIncentiveMultiplier;
+    /// @notice The absolute minimum ratio of collateral value to debt value used to calculate collateral requirements.
+    FixedPoint.Unsigned minimumCollateralizationRatio;
+    /// @notice The minimum USD value of an individual synthetic asset debt position.
+    FixedPoint.Unsigned minimumDebtValue;
+    /// @notice The collateralization ratio at which positions may be liquidated.
+    FixedPoint.Unsigned liquidationThreshold;
+    /// @notice Flag tells if there is a need to perform safety checks on user actions
+    bool safetyStateSet;
+    /// @notice asset -> action -> state
+    mapping(address => mapping(Action => SafetyState)) safetyState;
+    /* -------------------------------------------------------------------------- */
+    /*                              Collateral Assets                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Mapping of collateral asset token address to information on the collateral asset.
+    mapping(address => CollateralAsset) collateralAssets;
+    /**
+     * @notice Mapping of account -> asset -> deposit amount
+     */
+    mapping(address => mapping(address => uint256)) collateralDeposits;
+    /// @notice Mapping of account -> collateral asset addresses deposited
+    mapping(address => address[]) depositedCollateralAssets;
+    /* -------------------------------------------------------------------------- */
+    /*                                Kresko Assets                               */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Mapping of kresko asset token address to information on the Kresko asset.
+    mapping(address => KrAsset) kreskoAssets;
+    /// @notice Mapping of account -> krAsset -> debt amount owed to the protocol
+    mapping(address => mapping(address => uint256)) kreskoAssetDebt;
+    /// @notice Mapping of account -> addresses of borrowed krAssets
+    mapping(address => address[]) mintedKreskoAssets;
+    /// @notice The AMM oracle address.
+    address ammOracle;
+    /// @notice Offchain oracle decimals
+    uint8 extOracleDecimals;
 }
--- src/interfaces/IKresko.sol
+++ src/interfaces/IKresko.sol
@@ -1,10 +1,99 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.14;
 
-import {MinterInitArgs} from "../MinterTypes.sol";
+import {MinterInitArgs, KrAsset, CollateralAsset, MinterParams, Action, SafetyState} from "../MinterTypes.sol";
+import {FixedPoint} from "../libs/FixedPoint.sol";
+import {StabilityRateParams, StabilityRateConfig} from "../StabilityRateTypes.sol";
 
-interface IConfigurationFacet {
+interface IKresko {
     function initialize(MinterInitArgs calldata args) external;
+
+    function toggleAssetsPaused(
+        address[] memory _assets,
+        Action _action,
+        bool _withDuration,
+        uint256 _duration
+    ) external;
+
+    function safetyStateSet() external view returns (bool);
+
+    function safetyStateFor(
+        address _asset,
+        Action _action
+    ) external view returns (SafetyState memory);
+
+    function burnKreskoAsset(
+        address _account,
+        address _kreskoAsset,
+        uint256 _amount,
+        uint256 _mintedKreskoAssetIndex
+    ) external;
+
+    function batchCloseKrAssetDebtPositions(address _account) external;
+
+    function closeKrAssetDebtPosition(
+        address _account,
+        address _kreskoAsset
+    ) external;
+
+    function domainSeparator() external view returns (bytes32);
+
+    function minterInitializations() external view returns (uint256);
+
+    function feeRecipient() external view returns (address);
+
+    function ammOracle() external view returns (address);
+
+    function extOracleDecimals() external view returns (uint8);
+
+    function liquidationThreshold()
+        external
+        view
+        returns (FixedPoint.Unsigned memory);
+
+    function liquidationIncentiveMultiplier()
+        external
+        view
+        returns (FixedPoint.Unsigned memory);
+
+    function minimumCollateralizationRatio()
+        external
+        view
+        returns (FixedPoint.Unsigned memory);
+
+    function minimumDebtValue()
+        external
+        view
+        returns (FixedPoint.Unsigned memory);
+
+    function krAssetExists(address _krAsset) external view returns (bool);
+
+    function kreskoAsset(address _asset) external view returns (KrAsset memory);
+
+    function collateralAsset(
+        address _asset
+    ) external view returns (CollateralAsset memory);
+
+    function collateralExists(
+        address _collateralAsset
+    ) external view returns (bool);
+
+    function getAllParams() external view returns (MinterParams memory);
+
+    function getCollateralValueAndOraclePrice(
+        address _collateralAsset,
+        uint256 _amount,
+        bool _ignoreCollateralFactor
+    )
+        external
+        view
+        returns (FixedPoint.Unsigned memory, FixedPoint.Unsigned memory);
+
+    function getKrAssetValue(
+        address _kreskoAsset,
+        uint256 _amount,
+        bool _ignoreKFactor
+    ) external view returns (FixedPoint.Unsigned memory);
 
     function addCollateralAsset(
         address _collateralAsset,
@@ -104,4 +193,56 @@ interface IConfigurationFacet {
         uint256 _mintedKreskoAssetIndex,
         uint256 _depositedCollateralAssetIndex
     ) external;
+
+    function setupStabilityRateParams(
+        address _asset,
+        StabilityRateParams memory _setup
+    ) external;
+
+    function updateStabilityRateParams(
+        address _asset,
+        StabilityRateParams memory _setup
+    ) external;
+
+    function updateStabilityRateAndIndexForAsset(address _asset) external;
+
+    function updateKiss(address _kiss) external;
+
+    function repayStabilityRateInterestPartial(
+        address _account,
+        address _kreskoAsset,
+        uint256 _kissRepayAmount
+    ) external;
+
+    function repayFullStabilityRateInterest(
+        address _account,
+        address _kreskoAsset
+    ) external returns (uint256 kissRepayAmount);
+
+    function batchRepayFullStabilityRateInterest(
+        address _account
+    ) external returns (uint256 kissRepayAmount);
+
+    function getStabilityRateForAsset(
+        address _asset
+    ) external view returns (uint256 stabilityRate);
+
+    function getPriceRateForAsset(
+        address _asset
+    ) external view returns (uint256 priceRate);
+
+    function getDebtIndexForAsset(
+        address _asset
+    ) external view returns (uint256 debtIndex);
+
+    function getStabilityRateConfigurationForAsset(
+        address _asset
+    ) external view returns (StabilityRateConfig memory);
+
+    function kiss() external view returns (address);
+
+    function getLastDebtIndexForAccount(
+        address _account,
+        address _asset
+    ) external view returns (uint128 lastDebtIndex);
 }
