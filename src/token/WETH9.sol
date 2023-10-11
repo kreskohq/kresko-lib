03c4a9d72a24aa899ab55447b115f716c690dea8
panukettu <panu@0xp.lol>
2023-10-11 05:42:57 +0300

    chore: improve weth

--- src/token/WETH9.sol
+++ src/token/WETH9.sol
@@ -1,29 +1,34 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.8.21;
 import {ERC20} from "./ERC20.sol";
+import {SafeTransfer} from "./SafeTransfer.sol";
 
-contract WETH9 is ERC20 {
-    constructor() ERC20("Wrapped Ether", "WETH", 18) {
-        //
-    }
+contract WETH9 is ERC20("Wrapped Ether", "WETH", 18) {
+    using SafeTransfer for address payable;
 
     event Deposit(address indexed dst, uint256 wad);
     event Withdrawal(address indexed src, uint256 wad);
 
     function deposit() public payable virtual {
-        _balances[msg.sender] += msg.value;
+        _mint(msg.sender, msg.value);
+
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 wad) public virtual {
-        require(_balances[msg.sender] >= wad, "WETH9: Error");
-        _balances[msg.sender] -= wad;
-        payable(msg.sender).transfer(wad);
-        emit Withdrawal(msg.sender, wad);
+    function withdraw(uint256 amount) public virtual {
+        _burn(msg.sender, amount);
+
+        emit Withdrawal(msg.sender, amount);
+
+        payable(msg.sender).safeTransferETH(amount);
     }
 
     function totalSupply() public view override returns (uint256) {
         return address(this).balance;
+    }
+
+    receive() external payable virtual {
+        deposit();
     }
 }
