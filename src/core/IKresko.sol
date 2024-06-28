37e0983fd2ad9383b8dddf2e13141c5a3d3e910d
fxp3 <dev@0xp.lol>
2024-06-29 00:41:37 +0300

    chore: update asset state facet

--- src/core/IKresko.sol
+++ src/core/IKresko.sol
@@ -1032,45 +1032,66 @@ interface ICommonStateFacet {
 interface IAssetStateFacet {
     /**
      * @notice Get the state of a specific asset
+     * @param _assetAddr Address of the asset.
      * @return Asset State of asset
      * @custom:signature getAsset(address)
      * @custom:selector 0x30b8b2c6
      */
 
-    function getAsset(address) external view returns (Asset memory);
+    function getAsset(address _assetAddr) external view returns (Asset memory);
 
     /**
      * @notice Get price for an asset from address.
+     * @param _assetAddr Asset address.
      * @return uint256 Current price for the asset.
      * @custom:signature getPrice(address)
      * @custom:selector 0x41976e09
      */
-    function getPrice(address) external view returns (uint256);
+    function getPrice(address _assetAddr) external view returns (uint256);
 
     /**
      * @notice Get push price for an asset from address.
+     * @param _assetAddr Asset address.
      * @return RawPrice Current raw price for the asset.
      * @custom:signature getPushPrice(address)
      * @custom:selector 0xc72f3dd7
      */
-    function getPushPrice(address) external view returns (RawPrice memory);
+    function getPushPrice(
+        address _assetAddr
+    ) external view returns (RawPrice memory);
 
     /**
      * @notice Get value for an asset amount using the current price.
+     * @param _assetAddr Asset address.
+     * @param _amount The amount (uint256).
      * @return uint256 Current value for `_amount` of `_assetAddr`.
      * @custom:signature getValue(address,uint256)
      * @custom:selector 0xc7bf8cf5
      */
-    function getValue(address, uint256) external view returns (uint256);
+    function getValue(
+        address _assetAddr,
+        uint256 _amount
+    ) external view returns (uint256);
 
     /**
      * @notice Gets corresponding feed address for the oracle type and asset address.
-     * @return feed Feed address that the asset uses with the oracle type.
+     * @param _assetAddr The asset address.
+     * @param _oracleType The oracle type.
+     * @return feedAddr Feed address that the asset uses with the oracle type.
      */
     function getFeedForAddress(
-        address,
-        Enums.OracleType
-    ) external view returns (address feed);
+        address _assetAddr,
+        Enums.OracleType _oracleType
+    ) external view returns (address feedAddr);
+
+    /**
+     * @notice Get the market status for an asset.
+     * @param _assetAddr Asset address.
+     * @return bool True if the market is open, false otherwise.
+     * @custom:signature getMarketStatus(address)
+     * @custom:selector 0x3b3b3b3b
+     */
+    function getMarketStatus(address _assetAddr) external view returns (bool);
 }
 
 interface IAssetConfigFacet {
